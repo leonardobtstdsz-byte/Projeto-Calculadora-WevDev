63002f62f0db78c9e4c7462ead28baf7305f437d
Leonardo <leonardobtstdsz@gmail.com>
2026-03-14 20:02:07 +0000

    feat: modificando o compenente resultado

---
+++ src/components/Resultado.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+
+// Desestruturação Do resultado para pegar o valor
+const Tabela = ({resultado}) => {
+    const valor = parseFloat(resultado);
+
+    console.log(valor);
+
+    if (resultado <18.5){
+        return (
+        <>
+        <td>Abaixo do peso</td>
+        <td>Abaixo de 18.5</td>
+        </>
+        )
+    } else if (resultado >= 18.5 && resultado <25){
+        return (
+            <>
+            <td>Peso normal</td>
+            <td>18.5 - 24.9</td>
+            </>
+        )
+    } else if (resultado >= 25 && resultado <30){
+        return (
+            <>
+            <td>Sobrepeso</td>
+            <td>25 - 29.9</td>
+            </>
+        )
+    } else {
+        return (
+            <>
+            <td>Obesidade Grau-I</td>
+            <td>Maior ou igual a 40</td>
+            </>
+        )
+    }
+}
+
+const Resultado = ({resultado}) => {
+
+  return (
+    <div>
+        <div className='resultado'>
+            <h2>Seu imc é de: <span className='img-span'>{resultado}</span></h2>
+        </div>
+        <table className='tabela'>
+            <thead className='tabelaHeader'>
+                <tr>
+                    <th>Classificação</th>
+                    <th>IMC</th>
+                </tr>
+            </thead>
+            <tbody className='tabelaBody'>
+                <tr>
+                    {/* Chamando a tabela resultado */}
+                    <Tabela resultado={resultado}/>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default Resultado
