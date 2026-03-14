cd545f578b3f8277538983a9478381faa4c2a247
Leonardo <leonardobtstdsz@gmail.com>
2026-03-14 20:09:45 +0000

    feat: chamando os componenetes no app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header"
+import Resultado from "./components/Resultado"
+
+function App() {
+
+
+  return (
+    <>
+      <Header/>
+      <Resultado/>
+    </>
+  )
+}
+
+export default App
